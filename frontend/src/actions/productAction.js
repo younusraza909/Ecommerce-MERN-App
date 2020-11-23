e15d3ece6a4b8523c6e864cf6f70fef5d7eefb94
younusraza909 <younusraza909@gmail.com>
2020-11-23 10:43:37 -0800

    Making product list detail reducer and combine it with our app using hooks

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -2,6 +2,9 @@ import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_ERROR,
 	PRODUCT_LIST_SUCCESS,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_ERROR,
 } from "../constants/productConstants";
 
 import axios from "axios";
@@ -17,6 +20,23 @@ export const listProducts = () => async (dispatch) => {
 	} catch (error) {
 		dispatch({
 			type: PRODUCT_LIST_ERROR,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+	try {
+		dispatch({type: PRODUCT_DETAILS_REQUEST});
+
+		const {data} = await axios.get(`/api/products/${id}`);
+		dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data});
+	} catch (error) {
+		dispatch({
+			type: PRODUCT_DETAILS_ERROR,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
