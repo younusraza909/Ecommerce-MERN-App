74be77bf90b4d6b119c72cef56008b0018de8f1a
younusraza909 <younusraza909@gmail.com>
2020-11-25 08:27:23 -0800

    making middleware to check jwt token and verify it

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -25,4 +25,25 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
-export {authUser};
+//@desc    GET user profile
+//@route   GET /api/users/profile
+//@access  Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not Found");
+	}
+});
+
+export {authUser, getUserProfile};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 const router = express.Router();
+import {protect} from "../middleware/authMiddleware.js";
 
-import {authUser} from "../Controllers/userController.js";
+import {authUser, getUserProfile} from "../Controllers/userController.js";
 
 router.post("/login", authUser);
+
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
