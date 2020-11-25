d0fdbf3d8211b14d1092bb4f0e04dc72a3167f38
younusraza909 <younusraza909@gmail.com>
2020-11-25 09:20:49 -0800

    Making route to register a user and making middleware for mongoose to hash password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,15 @@ import express from "express";
 const router = express.Router();
 import {protect} from "../middleware/authMiddleware.js";
 
-import {authUser, getUserProfile} from "../Controllers/userController.js";
+import {
+	authUser,
+	getUserProfile,
+	registerUser,
+} from "../Controllers/userController.js";
 
 router.post("/login", authUser);
 
 router.route("/profile").get(protect, getUserProfile);
 
+router.route("/").post(registerUser);
 export default router;
