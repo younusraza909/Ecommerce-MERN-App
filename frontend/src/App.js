ead6df3e2eec40f1dd4f061569e02093bcae88ab
younusraza909 <younusraza909@gmail.com>
2020-11-27 19:16:52 -0800

    payment method screen created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 // import for bootstrap
@@ -20,6 +21,7 @@ function App() {
 				<main className='py-3'>
 					<Container>
 						<Route path='/login' component={LoginScreen} />
+						<Route path='/payment' component={PaymentMethodScreen} />
 						<Route path='/register' component={RegisterScreen} />
 						<Route path='/profile' component={ProfileScreen} />
 						<Route path='/product/:id' component={ProductScreen} />
