922b8dc832a82efb2c0db28e5c41d4a167ff0d3a
younusraza909 <younusraza909@gmail.com>
2020-11-30 20:19:46 -0800

    Order payment update endpoint with reducer and action for react frontend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,16 @@
 import express from "express";
 
-import {addOrderItem,getOrderById} from "../Controllers/orderController.js";
-import {protect} from "../middleware/authMiddleware.js";
+import {
+  addOrderItem,
+  getOrderById,
+  updateOrderToPaid,
+} from "../Controllers/orderController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, addOrderItem);
-router.route("/:id").get(protect,getOrderById)
+router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,57 +1,62 @@
-import {createStore, applyMiddleware, combineReducers} from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import {composeWithDevTools} from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 import {
-	productListReducer,
-	productDetailsReducer,
+  productListReducer,
+  productDetailsReducer,
 } from "./reducers/productReducers";
 import {
-	userLoginReducer,
-	userRegisterReducer,
-	userDetailsReducer,
-	userUpdateProfileReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
 } from "./reducers/userReducers";
-import {cartReducer} from "./reducers/cartReducers";
-import {orderCreateReducer,orderDetailsReducer} from "./reducers/orderRedcers";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+} from "./reducers/orderRedcers";
 
 const reducer = combineReducers({
-	productList: productListReducer,
-	productDetails: productDetailsReducer,
-	cart: cartReducer,
-	userLogin: userLoginReducer,
-	userRegister: userRegisterReducer,
-	userDetails: userDetailsReducer,
-	userUpdateProfile: userUpdateProfileReducer,
-	orderCreate: orderCreateReducer,
-	orderDetails:orderDetailsReducer
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
-	? JSON.parse(localStorage.getItem("cartItems"))
-	: [];
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
 const userInfoFromStorage = localStorage.getItem("userInfo")
-	? JSON.parse(localStorage.getItem("userInfo"))
-	: null;
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 
 const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
-	? JSON.parse(localStorage.getItem("shippingAddress"))
-	: {};
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
 
 const initialState = {
-	cart: {
-		cartItems: cartItemsFromStorage,
-		shippingAddress: shippingAddressFromStorage,
-	},
-	userLogin: {userInfo: userInfoFromStorage},
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
 
 const store = createStore(
-	reducer,
-	initialState,
-	composeWithDevTools(applyMiddleware(...middleware))
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
