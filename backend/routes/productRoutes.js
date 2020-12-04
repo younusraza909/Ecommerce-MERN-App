eaf4adf9d742f0554313dc68a1344f666e149891
younusraza909 <younusraza909@gmail.com>
2020-12-03 20:14:41 -0800

    Admin Create Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,18 +4,24 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from "../Controllers/productController.js";
 
 const router = express.Router();
 //@desc   Fetch all products
 //@route  GET /api/products
 //@access  Public
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 //@desc   Fetch  product with id
 //@route  GET /api/products/:id
 //@access  Public
 
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
