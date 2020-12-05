6a224f078302963038989c2a4411583577b62dff
younusraza909 <younusraza909@gmail.com>
2020-12-04 18:36:27 -0800

    Update product Functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,14 +9,8 @@ import {
 } from "../Controllers/productController.js";
 
 const router = express.Router();
-//@desc   Fetch all products
-//@route  GET /api/products
-//@access  Public
-router.route("/").get(getProducts).post(protect, admin, createProduct);
 
-//@desc   Fetch  product with id
-//@route  GET /api/products/:id
-//@access  Public
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 
 router
   .route("/:id")
